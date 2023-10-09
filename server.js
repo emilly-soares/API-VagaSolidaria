77d0b60819f9044d09980ab7abcd5890314d2413
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-08 20:06:19 -0400

    update server.js, create endPoint get e post

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-require('./configs/connection');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+require("./configs/connection");
+const User = require("./models/User");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(express.json(), cors());
-app.listen(port, () => { console.log(`Run server...${port}`) });
+app.listen(port, () => {
+  console.log(`Run server...${port}`);
+});
 
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await User.create({
+      email: email,
+      password: password,
+    }).then(() => res.status(200).json("success"));
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
