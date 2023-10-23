4d6ef6e20f033976f875d574adcf36794c21e750
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-22 21:24:13 -0400

    update userId

--- controllers/CandidateController.js
+++ controllers/CandidateController.js
@@ -1,15 +1,6 @@
 const Candidate = require("../models/Candidate");
 
 class CandidateController {
-  static async listCandidates(req, res) {
-    try {
-      const candidates = await Candidate.findAll();
-      res.json(candidates);
-    } catch (err) {
-      res.status(404).send(err);
-    }
-  }
-
   static async createCandidate(req, res) {
     try {
       const {
@@ -21,6 +12,7 @@ class CandidateController {
         neighborhood,
         userId,
       } = req.body;
+
       await Candidate.create({
         name: name,
         dateBirth: dateBirth,
@@ -29,9 +21,68 @@ class CandidateController {
         numberStreet: numberStreet,
         neighborhood: neighborhood,
         userId: parseInt(userId),
-      }).then(() => res.status(200).json("success"));
-    } catch (err) {
-      res.status(404).send(err);
+      });
+
+      return res.status(201).json({ message: "Candidato criado com sucesso" });
+    } catch (error) {
+      console.error("Erro ao criar candidato:", error);
+      res.status(500).json({ error: "Erro ao criar candidato" });
+    }
+  }
+
+  static async listCandidates(req, res) {
+    try {
+      const candidates = await Candidate.findAll();
+      return res.status(200).json(candidates);
+    } catch (error) {
+      console.error("Erro ao listar candidatos:", error);
+      res.status(500).json({ error: "Erro ao listar candidatos" });
+    }
+  }
+
+  static async deleteCandidate(req, res) {
+    const userId = req.params.userId;
+    try {
+      await Candidate.destroy({
+        where: {
+          userId: userId,
+        },
+      });
+      return res
+        .status(200)
+        .json({ message: "Candidato excluído com sucesso" });
+    } catch (error) {
+      console.error("Erro ao excluir candidato:", error);
+      res.status(500).json({ error: "Erro ao excluir candidato" });
+    }
+  }
+
+  static async updateCandidate(req, res) {
+    const userId = req.params.userId;
+    const updatedCandidateData = req.body;
+
+    try {
+      const candidate = await Candidate.findByPk(userId);
+
+      if (!candidate) {
+        return res.status(404).json({ error: "Candidato não encontrado" });
+      }
+
+      await candidate.update({
+        name: updatedCandidateData.name,
+        dateBirth: updatedCandidateData.dateBirth,
+        CPF: updatedCandidateData.CPF,
+        street: updatedCandidateData.street,
+        numberStreet: updatedCandidateData.numberStreet,
+        neighborhood: updatedCandidateData.neighborhood,
+      });
+
+      return res
+        .status(200)
+        .json({ message: "Candidato atualizado com sucesso" });
+    } catch (error) {
+      console.error("Erro ao atualizar candidato:", error);
+      res.status(500).json({ error: "Erro ao atualizar candidato" });
     }
   }
 }
