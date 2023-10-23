1c2ad07223b3144694c1920c363c5d2b39251d86
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-22 21:40:30 -0400

    update final routes user and candidate

--- routes/routes.js
+++ routes/routes.js
@@ -3,15 +3,14 @@ const routes = express.Router();
 const CandidateController = require("../controllers/CandidateController");
 const UserController = require("../controllers/UserController");
 
-routes.post("/user", UserController.createUser);
-routes.get("/users", UserController.listUsers);
-routes.put("/user/:id", UserController.updateUser);
-routes.delete("/user/:id", UserController.deleteUser);
-
-
 routes.post("/candidate", CandidateController.createCandidate);
 routes.get("/candidates", CandidateController.listCandidates);
 routes.put("/candidate/:userId", CandidateController.updateCandidate);
 routes.delete("/candidate/:userId", CandidateController.deleteCandidate);
+
+routes.post("/user", UserController.createUser);
+routes.get("/users", UserController.listUsers);
+routes.put("/user/:id", UserController.updateUser);
+routes.delete("/user/:id", UserController.deleteUser);
 
 module.exports = routes;
