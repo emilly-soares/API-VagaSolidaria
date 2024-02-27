2aac46f9c46a2d569f99dc15ce4156a54fc8f875
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2024-02-27 10:36:34 -0400

    insert migration removeName from tb_candidates

---
+++ migrations/20240227131427-removeNameFromCandidate.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('tb_candidates', 'name');
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('tb_candidates', 'name', {
+      type: Sequelize.STRING,
+      allowNull: true, 
+    });
+  }
+};
